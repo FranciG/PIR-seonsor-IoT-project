15547903b44ffe8e737fb4cc47e1f799226a583d
t7haki01 <t7haki01@students.oamk.fi>
2019-10-11 09:47:50 +0300

    daily chart done

--- counter/urls.py
+++ counter/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.show, name='index'),
     path('data/', views.DataView.show, name='data'),
-    path('data/ex', views.DataView.data_analysis1, name='data-analysis1'),
-    path('stream/', views.StreamView.show, name='stream'),
+    path('data/daily', views.DataView.daily, name='daily'),
+    path('data/hourly', views.DataView.hourly, name='hourly'),
 ]
--- counter/views.py
+++ counter/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render, render_to_response
+
 from django.http import HttpResponseRedirect
-from django.views import generic
+
 from django.http import HttpResponse
+
 from .models import Counter
 import numpy as np
 import pandas as pd
-from django_pandas.managers import DataFrameManager
-from django.db import connection
-import matplotlib.pyplot as plt
-import matplotlib as mat
-from plotly.offline import plot
-import plotly.graph_objs as go
-from matplotlib.backends.backend_agg import FigureCanvasAgg
+
 import io
 from bokeh.embed import components
 from bokeh.plotting import figure, output_file, show
@@ -23,46 +19,71 @@ class IndexView():
         return render(request, template_name)
 
 class DataView():
-
+    
     def show(request):
         template_name = 'counter/data.html'
         return render(request, template_name)
 
-    def data_analysis1(request):
-        template_name = 'counter/data-analysis1.html'
+    def daily(request):
+        template_name = 'counter/data.html'
 
         data = list(Counter.objects.all().values())
 
         df = pd.DataFrame(data)
 
         df.record=pd.to_datetime(df.record)
-        timeData = df.groupby([pd.Grouper(key='record',freq='H'), df.passed]).size().reset_index(name='count')
 
-        test = df
-        y = timeData["count"]
+        # df['hour'] = df['record'].apply(lambda x: x.hour)
+
+        timeData = df.groupby([pd.Grouper(key='record',freq='d'), df.passed]).size().reset_index(name='dailyCount')
+
+        y = timeData["dailyCount"]
+        
         x = timeData["record"]
 
-        plot = figure(title="basic", x_axis_label="x",x_axis_type="datetime", y_axis_label="y",plot_width=400,plot_height=400)
+        plot = figure(title="Daily", x_axis_label="Date",x_axis_type="datetime", y_axis_label="Number of Counts",plot_width=400,plot_height=400)
+        
         plot.line(x,y,line_width=2)
+        
         script,div = components(plot)
-        return render_to_response(template_name,{'script':script, 'div':div, 'test':test})
+
+        return render_to_response(template_name,{'script':script, 'div':div})
+
+    def hourly(request):
+        template_name = 'counter/hourly.html'
+
+        df = DataView.get_data()
+
+        df.record=pd.to_datetime(df.record)
+
+        recordDates = df.record.dt.strftime('%d-%m-%y').unique()
+
+        return render_to_response(template_name,{'dates':recordDates})
+
+        # df = DataView.get_data()
+
+        # df.record=pd.to_datetime(df.record)
+
+        # timeData = df.groupby([pd.Grouper(key='record',freq='H'), df.passed]).size().reset_index(name='hourlyCount')
+
+        # y = timeData["count"]
+
+        # x = timeData["record"]
+
+        # plot = figure(title="Daily", x_axis_label="Date",x_axis_type="datetime", y_axis_label="Number of Counts",plot_width=400,plot_height=400)
+
+        # plot.line(x,y,line_width=2)
+
+        # script,div = components(plot)
+
+        # return render_to_response(template_name,{'script':script, 'div':div})
 
     def get_data():
         data = list(Counter.objects.all().values())
-        df = pd.DataFrame(list(Counter.objects.all().values()))
-        
-        # data = Counter.objects.all()
-        # context = {'data': data}
 
-        # query = str(Counter.objects.all().query)
-        # df = pd.read_sql_query(query, connection)
-
+        df = pd.DataFrame(data)
+    
         return df
-
-class StreamView():
-    def show(request):
-        template_name = 'counter/stream.html'
-        return render(request, template_name)
 
 
 
