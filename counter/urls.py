f75dd7db678268d58d1490fc62417f8dcf8e742c
t7haki01 <t7haki01@students.oamk.fi>
2019-10-14 10:27:26 +0300

    hourly added

--- counter/urls.py
+++ counter/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'counter'
+
 urlpatterns = [
     path('', views.IndexView.show, name='index'),
     path('data/', views.DataView.show, name='data'),
     path('data/daily', views.DataView.daily, name='daily'),
     path('data/hourly', views.DataView.hourly, name='hourly'),
+    path('data/hourlyReq', views.DataView.hourlyReq, name='hourlyReq'),
 ]
--- counter/views.py
+++ counter/views.py
@@ -11,7 +11,7 @@ import pandas as pd
 import io
 from bokeh.embed import components
 from bokeh.plotting import figure, output_file, show
-import datetime
+from datetime import datetime
 
 class IndexView():
     def show(request):
@@ -31,9 +31,9 @@ class DataView():
 
         df = pd.DataFrame(data)
 
-        df.record=pd.to_datetime(df.record)
+        df.record = pd.to_datetime(df.record)
 
-        # df['hour'] = df['record'].apply(lambda x: x.hour)
+        df['hour'] = df['record'].apply(lambda x: x.hour)
 
         timeData = df.groupby([pd.Grouper(key='record',freq='d'), df.passed]).size().reset_index(name='dailyCount')
 
@@ -49,34 +49,67 @@ class DataView():
 
         return render_to_response(template_name,{'script':script, 'div':div})
 
+    def every10Min(df, selectedDate):
+        timeData = df.groupby([pd.Grouper(key='record',freq='10min'), df.passed]).size().reset_index(name='10MinCount')
+        timeData['date'] = timeData.record.dt.strftime('%y-%m-%d')
+        picked = str(selectedDate.year) + "-" + str(selectedDate.month) + "-" + str(selectedDate.day)
+        hoursArray = timeData[(timeData['date'] == picked)].record.dt.strftime('%H:%M')
+        counts = timeData[(timeData['date'] == picked)]['10MinCount']
+
+        x = hoursArray
+        y = counts
+        
+        plot = figure(title="Counts per Every 10 min", x_axis_label="Time / Hr:Min", y_axis_label="Number of Counts",plot_width=400,plot_height=400)
+        
+        plot.line(x,y,line_width=2)
+        return components(plot)
+    
     def hourly(request):
         template_name = 'counter/hourly.html'
 
-        df = DataView.get_data()
-
-        df.record=pd.to_datetime(df.record)
-
-        recordDates = df.record.dt.strftime('%d-%m-%y').unique()
-
-        return render_to_response(template_name,{'dates':recordDates})
-
         # df = DataView.get_data()
 
-        # df.record=pd.to_datetime(df.record)
+        selectedDate = '2019-10-09 00:00:00'
 
-        # timeData = df.groupby([pd.Grouper(key='record',freq='H'), df.passed]).size().reset_index(name='hourlyCount')
+        if(request.method == 'POST'):
+            selectedDate = request.POST['date'] + " 00:00:00"
 
-        # y = timeData["count"]
+        selectedDate = datetime.strptime(selectedDate, "%Y-%m-%d %H:%M:%S")
+        
+        data = DataHandler.get_mysql_data()
 
-        # x = timeData["record"]
+        df = DataHandler.more_field(data)
 
-        # plot = figure(title="Daily", x_axis_label="Date",x_axis_type="datetime", y_axis_label="Number of Counts",plot_width=400,plot_height=400)
+        dt = df[(df['year']==int(selectedDate.year) ) & (df['month']== int(selectedDate.month) ) & (df['day']== int(selectedDate.day) )]
 
-        # plot.line(x,y,line_width=2)
+        hrArray = dt['hour'].unique()
+        hourlyCount = []
 
-        # script,div = components(plot)
+        for hr in hrArray:
+            count = df[(df['year']==  int(selectedDate.year) ) & (df['month']== int(selectedDate.month) ) & (df['day']== int(selectedDate.day) ) & (df['hour']==hr )]['passed'].count()
+            hourlyCount.append(count)
 
-        # return render_to_response(template_name,{'script':script, 'div':div})
+        x = hrArray
+        y = hourlyCount
+
+        chartTitle = dt.record.dt.strftime('%d-%m-%y').unique()[0] + " Hourly"
+
+        plot = figure(title=chartTitle, x_axis_label="Hour", y_axis_label="Number of Counts",plot_width=400,plot_height=400)
+
+        plot.line(x,y,line_width=2)
+
+        script,div = components(plot)
+
+        script2,div2 = DataView.every10Min(df, selectedDate)
+
+        contents = {'script':script, 'div':div, 'script2':script2, 'div2':div2}
+
+        return render_to_response(template_name, contents)
+
+    def hourlyReq(request):
+        template_name = 'counter/hourlyReq.html'
+        return render(request, template_name)
+
 
     def get_data():
         data = list(Counter.objects.all().values())
@@ -86,6 +119,26 @@ class DataView():
         return df
 
 
+class DataHandler():
+    def get_mysql_data():
+        data = list(Counter.objects.all().values())
+        return data
 
+    def get_data():
+        data = list(Counter.objects.all().values())
+
+        df = pd.DataFrame(data)
+    
+        return df
+
+    def more_field(data):
+        df = pd.DataFrame(data)
+        df.record = pd.to_datetime(df.record)
+        df['hour'] = df['record'].apply(lambda x: x.hour)
+        df['day'] = df['record'].apply(lambda x: x.day)
+        df['month'] = df['record'].apply(lambda x: x.month)
+        df['year'] = df['record'].apply(lambda x: x.year)
+        # df['date'] = df['record'].apply(lambda x: (x.day,x.month,x.year) )
+        return df
 
 
